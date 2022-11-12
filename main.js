feb1c126e65b691f6659c08732b6e53a58a336ce
AlessandroDeFa <aledefaale@gmail.com>
2022-11-12 21:46:56 +0100

    added a dinamic reviews section

--- main.js
+++ main.js
@@ -77,3 +77,79 @@ tlcontact.from('.title-contact', {y:100, opacity: 0, duration:1})
 .from('.form-mail', {opacity:0, x: 100, duration:1,ease: Power4.easeOut}, '-=0.99')
 .from('.form-message', {opacity:0, y: 100, duration:1,ease: Power4.easeOut}, '-=.6')
 .from('.container-button', {opacity:0, duration:1, y:100,ease: Power4.easeOut}, '-=0.99')
+
+
+// Animation slide reviews
+
+const  reviews = [ 
+  {
+    id: 1,
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam, dolorem. Lorem ipsum dolor sit ametLorem ipsum dolor sit amet consectetur adipisicing elit.",
+    name: "- JACOB",
+  },
+  {
+    id: 2,
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam, dolorem..",
+    name: "- DANIEL",
+  },
+  {
+    id: 3,
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam, dolorem. Lorem ipsum dolor sit ametLorem ipsum dolor sit amet consectetur ",
+    name: "- TYNA",
+  },
+  {
+    id: 4,
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam, dolorem.",
+    name: "- GISELLE",
+  },
+  {
+    id: 5,
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam, dolorem. Lorem ipsum dolor sit ametLorem ipsum dolor sit amet consectetur ametLorem ipsum dolor sit amet consectetur  ",
+    name: "- MARY",
+  }
+]
+
+const slides = document.querySelectorAll('.slide');
+const textreview = document.querySelector('#text-review');
+const author = document.querySelector('#author');
+const nextbutton = document.querySelector('.nextbutton');
+const prevbutton = document.querySelector('.prevbutton');
+
+// Set Starting item 
+
+var currentItem = 0;
+
+// load initial item 
+
+window.addEventListener('DOMContentLoaded', function(){
+  const item = reviews[currentItem];
+  author.textContent = item.name;
+  textreview.textContent = item.text;
+});
+
+// show persone based on item 
+
+function showPerson(person){
+  const item = reviews[person];
+  author.textContent = item.name;
+  textreview.textContent = item.text;
+}
+
+//show next person
+
+nextbutton.addEventListener("click", function () {
+  currentItem++;
+  if (currentItem > reviews.length - 1) {
+    currentItem = 0;
+  }
+  showPerson(currentItem);
+});
+
+// show prev person
+prevbutton.addEventListener("click", function () {
+  currentItem--;
+  if (currentItem < 0) {
+    currentItem = reviews.length - 1;
+  }
+  showPerson(currentItem);
+});
